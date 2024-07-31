f1b374b29d72f634ca5dad256ee21713d011c19a
rxgoodness <adedokuntoyyib@gmail.com>
2024-07-31 21:48:56 +0100

    update

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,34 +1,11 @@
-// import { connectDB } from "./config/database";
-// connectDB();
-// import express from "express";
-// const app = express();
-// import { config } from "./config/env";
-// const { APP_NAME } = config;
-
-// app.use(express.urlencoded({ extended: true, limit: "100mb" }));
-// app.use(express.json({ limit: "100mb" }));
-
-// // import routers
-
-// // use routers
-
-// app.use("/", async (req, res) => {
-//   res.status(200).send({
-//     message: `hi ${APP_NAME} here, 👋`,
-//   });
-// });
-
-// export default app;
-
-
 import express from 'express';
-import mongoose from 'mongoose';
 import { initializeRabbitMQ } from './services/MicroserviceConnections/initializeRabbitMQ';
 import { connectDB } from './config/database';
 import authRoutes from './routes/auth';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(express.json());
 
 // use routers
